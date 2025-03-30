1c4fe0465a45e4e2ebbefab8a2070d5f1900ee39
Rustam Shrestha <santosh.787402@smc.tu.edu.np>
2025-03-30 16:36:07 +0545

    set up the project and database connection alongside Model view and controller has been set up correctly with route and /product endpoint will be having get request and show basic page

---
+++ crud-app/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//the get request on /product endpoint  
+//parameter 2: loscation of controller
+//function name of the controller
+// with name of the route we specify only name of that route
+Route::get('/product', [ProductController::class, 'index'])->name('products.index');
